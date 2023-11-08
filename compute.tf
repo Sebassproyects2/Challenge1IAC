d8a8ea0a3e9b3714ade48d20147a414eea91d79a
Sebassproyects2 <sebassolis2000@gmail.com>
2023-11-08 09:15:24 -0600

    bloque para la app

--- compute.tf
+++ compute.tf
@@ -6,6 +6,21 @@ resource "aws_instance" "SebasInstancia" {
   subnet_id = aws_subnet.challengeSubnet.id
   vpc_security_group_ids = [aws_security_group.challengeSecutiry.id]
 
+    user_data = <<EOF
+#!/bin/bash
+sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+cd /var/www/
+sudo chgrp ubuntu html
+sudo chown ubuntu html
+cd html
+wget https://challengeiac.s3.amazonaws.com/binaries2.zip
+sudo apt-get install unzip
+unzip binaries.zip
+EOF
+
   tags = {
     Name = "challenge_Instance"
   }
