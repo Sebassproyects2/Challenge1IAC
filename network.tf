0d1b30319bb7c4eca15a81db149d3e35e55e9f8f
Sebassproyects2 <sebassolis2000@gmail.com>
2023-11-08 10:03:23 -0600

    map añadido

--- network.tf
+++ network.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "challengeVPC" {
 resource "aws_subnet" "challengeSubnet" {
   vpc_id     = aws_vpc.challengeVPC.id
   cidr_block = "10.0.1.0/24"
-  //map_public_ip_on_launch = true
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "sebas_challenge_Subnet"
