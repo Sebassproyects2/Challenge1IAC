9dad1acc4ef76a2d71ebe8904af63686d29904a1
Sebassproyects2 <sebassolis2000@gmail.com>
2023-11-08 11:15:48 -0600

    correccion, abierto para internet

--- security.tf
+++ security.tf
@@ -5,8 +5,8 @@ resource "aws_security_group" "challengeSecutiry" {
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
