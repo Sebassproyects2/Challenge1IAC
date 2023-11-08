4ae559a41c0d23d903d802b8ccfb42f737c8941a
Sebassproyects2 <sebassolis2000@gmail.com>
2023-11-08 10:15:33 -0600

    modificaciones network y variables

--- network.tf
+++ network.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "challengeVPC" {
 
 resource "aws_subnet" "challengeSubnet" {
   vpc_id     = aws_vpc.challengeVPC.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.variSubnet
   map_public_ip_on_launch = true
 
   tags = {
--- variables.tf
+++ variables.tf
@@ -4,3 +4,10 @@ variable "variSecurityC" {
   default = "SecurityGroupT2"
 
 }
+
+variable "variSubnet" {
+  type = string
+  description = "VPC"
+  default = "10.0.1.0/24"
+
+}
